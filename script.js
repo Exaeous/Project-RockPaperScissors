2aab5dcfe9299bae1070d80b7a860888d9534aa6
Sam <exaeous@gmail.com>
2025-04-04 22:43:15 +0530

    Add logic to get Computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// Logic for Computer choice
+const computersChoices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  let computerChoice =
+    computersChoices[Math.floor(Math.random() * computersChoices.length)];
+  return computerChoice;
+}
+
+getComputerChoice();
