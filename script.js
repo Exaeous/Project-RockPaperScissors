653b35e8f49c8fd068ec4fbaf207ad9e59cff38a
Sam <exaeous@gmail.com>
2025-04-05 21:02:19 +0530

    Add logic to play the game once

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
 
-let computerScore = 0;
+// Scores
 let humanScore = 0;
+let computerScore = 0;
+
+// Logic to get Humans choice
+function getHumanChoice() {
+  return prompt("Enter your choice");
+}
 
 // Logic to get Computers choice
 const computersChoices = ["rock", "paper", "scissors"];
@@ -10,11 +16,38 @@ function getComputerChoice() {
   return computersChoices[Math.floor(Math.random() * computersChoices.length)];
 }
 
-// console.log(getComputerChoice());
-
-// Logic to get Humans choice
-function getHumanChoice() {
-  return prompt("Enter your choice");
+// Logic to play the game
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+    console.log("You lose! Paper beats Rock");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    console.log("You lose! Scissors beats paper");
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    console.log("You lose! Rock beats Scissors");
+  } else if (computerChoice === "rock" && humanChoice === "paper") {
+    humanScore++;
+    console.log("You win! Paper beats Rock");
+  } else if (computerChoice === "paper" && humanChoice === "scissors") {
+    humanScore++;
+    console.log("You win! Scissors beats paper");
+  } else if (computerChoice === "scissors" && humanChoice === "rock") {
+    humanScore++;
+    console.log("You win! Rock beats Scissors");
+  } else {
+    console.log("It's a draw");
+  }
 }
 
-// console.log(getHumanChoice());
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+console.log(`Human plays ${humanSelection}`);
+console.log(`Computer plays ${computerSelection}`);
+
+playRound(humanSelection, computerSelection);
+
+console.log(`Human score is ${humanScore}`);
+console.log(`Computer score is ${computerScore}`);
