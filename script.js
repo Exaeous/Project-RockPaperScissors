115175dadb033ed1106dd595394b6a95c5667d0f
Sam <exaeous@gmail.com>
2025-05-29 22:48:31 +0530

    Add logic to declare winner and end the game

--- script.js
+++ script.js
@@ -31,28 +31,34 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-    showComputersScore();
     result.textContent = "You lose! Paper beats Rock";
+    showComputersScore();
+    declareWinner();
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore++;
-    showComputersScore();
     result.textContent = "You lose! Scissors beats paper";
+    showComputersScore();
+    declareWinner();
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++;
-    showComputersScore();
     result.textContent = "You lose! Rock beats Scissors";
+    showComputersScore();
+    declareWinner();
   } else if (computerChoice === "rock" && humanChoice === "paper") {
     humanScore++;
-    showHumansScore();
     result.textContent = "You win! Paper beats Rock";
+    showHumansScore();
+    declareWinner();
   } else if (computerChoice === "paper" && humanChoice === "scissors") {
     humanScore++;
-    showHumansScore();
     result.textContent = "You win! Scissors beats paper";
+    showHumansScore();
+    declareWinner();
   } else if (computerChoice === "scissors" && humanChoice === "rock") {
     humanScore++;
-    showHumansScore();
     result.textContent = "You win! Rock beats Scissors";
+    showHumansScore();
+    declareWinner();
   } else {
     result.textContent = "It's a draw";
   }
@@ -70,4 +76,23 @@ function showHumansScore() {
 let computersScore = document.querySelector(".computer-score");
 function showComputersScore() {
   computersScore.textContent = computerScore;
+}
+
+// Ending the game
+function disableAllButtons() {
+  const buttons = document.getElementsByTagName("button");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+}
+
+// Declaring the winner and disabling the buttons
+function declareWinner() {
+  if (humanScore === 5 && computerScore < 5) {
+    result.textContent = "You win";
+    disableAllButtons();
+  } else if (computerScore === 5 && humanScore < 5) {
+    result.textContent = "Computer win's";
+    disableAllButtons();
+  }
 }
